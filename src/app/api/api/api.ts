03d875c266dbcf1a4621d30c9c56efd41f572d97
David Paiva <david.paiva+github@my.istec.pt>
2023-10-25 22:32:44 +0100

    Add base API

---
+++ src/app/api/api/api.ts
@@ -0,0 +1,20 @@
+export * from './community.service';
+import { CommunityService } from './community.service';
+export * from './follower.service';
+import { FollowerService } from './follower.service';
+export * from './like.service';
+import { LikeService } from './like.service';
+export * from './post.service';
+import { PostService } from './post.service';
+export * from './user.service';
+import { UserService } from './user.service';
+export * from './userCommunities.service';
+import { UserCommunitiesService } from './userCommunities.service';
+export const APIS = [
+  CommunityService,
+  FollowerService,
+  LikeService,
+  PostService,
+  UserService,
+  UserCommunitiesService,
+];
