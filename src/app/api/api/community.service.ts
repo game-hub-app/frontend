b0392ea8977549ed2c473818f2c470cfdda77137
David Paiva <david.paiva+github@my.istec.pt>
2024-01-19 11:31:08 +0000

    Implement GetCommunityPosts

--- src/app/api/api/community.service.ts
+++ src/app/api/api/community.service.ts
@@ -23,6 +23,7 @@ import { CustomHttpUrlEncodingCodec } from '../encoder';
 import { Observable } from 'rxjs';
 
 import { Community } from '../model/community';
+import { Post } from '../model/post';
 import { UserCommunities } from '../model/userCommunities';
 
 import { BASE_PATH, COLLECTION_FORMATS } from '../variables';
@@ -286,6 +287,68 @@ export class CommunityService {
     return this.httpClient.request<Array<UserCommunities>>(
       'get',
       `${this.basePath}/Community/${encodeURIComponent(String(id))}/Members`,
+      {
+        withCredentials: this.configuration.withCredentials,
+        headers: headers,
+        observe: observe,
+        reportProgress: reportProgress,
+      }
+    );
+  }
+
+  /**
+   *
+   *
+   * @param id
+   * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+   * @param reportProgress flag to report request and response progress.
+   */
+  public communityIdPostsGet(
+    id: string,
+    observe?: 'body',
+    reportProgress?: boolean
+  ): Observable<Array<Post>>;
+  public communityIdPostsGet(
+    id: string,
+    observe?: 'response',
+    reportProgress?: boolean
+  ): Observable<HttpResponse<Array<Post>>>;
+  public communityIdPostsGet(
+    id: string,
+    observe?: 'events',
+    reportProgress?: boolean
+  ): Observable<HttpEvent<Array<Post>>>;
+  public communityIdPostsGet(
+    id: string,
+    observe: any = 'body',
+    reportProgress: boolean = false
+  ): Observable<any> {
+    if (id === null || id === undefined) {
+      throw new Error(
+        'Required parameter id was null or undefined when calling communityIdPostsGet.'
+      );
+    }
+
+    let headers = this.defaultHeaders;
+
+    // to determine the Accept header
+    let httpHeaderAccepts: string[] = [
+      'text/plain',
+      'application/json',
+      'text/json',
+    ];
+    const httpHeaderAcceptSelected: string | undefined =
+      this.configuration.selectHeaderAccept(httpHeaderAccepts);
+    if (httpHeaderAcceptSelected != undefined) {
+      headers = headers.set('Accept', httpHeaderAcceptSelected);
+    }
+
+    // to determine the Content-Type header
+    const consumes: string[] = [];
+
+    return this.httpClient.request<Array<Post>>(
+      'get',
+      `${this.basePath}/Community/${encodeURIComponent(String(id))}/Posts`,
       {
         withCredentials: this.configuration.withCredentials,
         headers: headers,
