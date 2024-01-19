0c769915846da6210325807fc2de96dc50fcb06c
David Paiva <david.paiva+github@my.istec.pt>
2024-01-19 11:24:47 +0000

    Add Community Search and User Search

--- src/app/api/api/user.service.ts
+++ src/app/api/api/user.service.ts
@@ -733,4 +733,68 @@ export class UserService {
       }
     );
   }
+
+  /**
+   *
+   *
+   * @param query
+   * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+   * @param reportProgress flag to report request and response progress.
+   */
+  public userSearchGet(
+    query?: string,
+    observe?: 'body',
+    reportProgress?: boolean
+  ): Observable<Array<User>>;
+  public userSearchGet(
+    query?: string,
+    observe?: 'response',
+    reportProgress?: boolean
+  ): Observable<HttpResponse<Array<User>>>;
+  public userSearchGet(
+    query?: string,
+    observe?: 'events',
+    reportProgress?: boolean
+  ): Observable<HttpEvent<Array<User>>>;
+  public userSearchGet(
+    query?: string,
+    observe: any = 'body',
+    reportProgress: boolean = false
+  ): Observable<any> {
+    let queryParameters = new HttpParams({
+      encoder: new CustomHttpUrlEncodingCodec(),
+    });
+    if (query !== undefined && query !== null) {
+      queryParameters = queryParameters.set('query', <any>query);
+    }
+
+    let headers = this.defaultHeaders;
+
+    // to determine the Accept header
+    let httpHeaderAccepts: string[] = [
+      'text/plain',
+      'application/json',
+      'text/json',
+    ];
+    const httpHeaderAcceptSelected: string | undefined =
+      this.configuration.selectHeaderAccept(httpHeaderAccepts);
+    if (httpHeaderAcceptSelected != undefined) {
+      headers = headers.set('Accept', httpHeaderAcceptSelected);
+    }
+
+    // to determine the Content-Type header
+    const consumes: string[] = [];
+
+    return this.httpClient.request<Array<User>>(
+      'get',
+      `${this.basePath}/User/Search`,
+      {
+        params: queryParameters,
+        withCredentials: this.configuration.withCredentials,
+        headers: headers,
+        observe: observe,
+        reportProgress: reportProgress,
+      }
+    );
+  }
 }
